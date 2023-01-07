16a2fe2bc9e70c951fd3057861c2404e61104fd1
Ben Gardiner <bengardiner18@gmail.com>
2023-01-06 22:53:17 -0600

    finished game with input and random colors

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 
-let width = 40;
-let height = 20;
 
 
+let height, width;
+let button = document.querySelector(".generate");
+
+button.addEventListener("click", () => {
+
+    height = parseInt(document.querySelector("#height").value);
+    width = parseInt(document.querySelector("#width").value);
+
+    createTable(width,height);
+
+});
+
+function getRandomColor() {
+    let randRed = Math.random()*256;
+    let randGreen = Math.random()*256;
+    let randBlue = Math.random()*256;
+
+    return [randRed,randGreen,randBlue];
+}
 
 function createTable(width,height) {
+
 
     // general board object
     const board = document.querySelector(".board");
@@ -13,23 +31,34 @@ function createTable(width,height) {
         `display:grid; gap:5px; grid-template-columns:repeat(${width},1/${width}fr); grid-template-rows:repeat(${width},1/${width}fr`
     );
 
+    if (width == 0 || height == 0) {
+        board.innerHTML = "";
+    }
+    else if (width > 100 || height > 100) {
+        alert("grid dimensions are too large pick something smaller!")
+    }
+    else if (width < 0 || height < 0) {
+        alert("grid dimensions must be positive integers");
+    }
+    else {
+        // reseting board before adding more squares
+        board.innerHTML = "";
+        for (let i = 0; i < height; i ++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            for (let j = 0; j < width; j++) {
+                let square = document.createElement("div");
+                square.classList.add("square");
     
-    for (let i = 0; i < height; i ++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        for (let j = 0; j < width; j++) {
-            let square = document.createElement("div");
-            square.classList.add("square");
-
-            square.addEventListener("click", () => {
-                square.style.backgroundColor = "red";
-            })
-
-            row.append(square);
-            
+                square.addEventListener("click", () => {
+                    color = getRandomColor();
+                    square.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`;
+                })
+    
+                row.append(square);
+                
+            }
+            board.append(row);
         }
-        board.append(row);
     }
 }
-
-createTable(width,height);
